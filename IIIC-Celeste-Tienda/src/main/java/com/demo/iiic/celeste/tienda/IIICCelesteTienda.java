073a23e2490e65f55d492e34fa691ae5f08d25b1
celeste9192 <108299741+celeste9192@users.noreply.github.com>
2023-09-13 20:52:02 -0600

    hola

---
+++ IIIC-Celeste-Tienda/src/main/java/com/demo/iiic/celeste/tienda/IIICCelesteTienda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.demo.iiic.celeste.tienda;
+
+/**
+ *
+ * @author MARÍA CELESTE
+ */
+public class IIICCelesteTienda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
