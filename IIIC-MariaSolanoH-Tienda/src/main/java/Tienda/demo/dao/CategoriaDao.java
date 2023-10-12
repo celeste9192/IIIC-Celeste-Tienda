3b7b25f5713c4e1329b15d40125bc5a4618c797e
celeste9192 <108299741+celeste9192@users.noreply.github.com>
2023-10-11 19:24:08 -0600

    antes de recreo

---
+++ IIIC-MariaSolanoH-Tienda/src/main/java/Tienda/demo/dao/CategoriaDao.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tienda.demo.dao;
+
+
+import Tienda.demo.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+/**
+ *
+ * @author MARÍA CELESTE
+ * 
+ * 
+ * 
+ */
+
+public interface CategoriaDao extends JpaRepository <Categoria,Long>{
+    
+}
+
+    
+
