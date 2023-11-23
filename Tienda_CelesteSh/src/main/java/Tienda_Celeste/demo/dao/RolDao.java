19862a3c7ff160bd83f2762b6194bc05fef0bcf9
celeste9192 <108299741+celeste9192@users.noreply.github.com>
2023-11-22 19:58:49 -0600

    semana 11
    Profe tuve que cambiar la contraseña y usuario de la base de datos, pero no se porque

---
+++ Tienda_CelesteSh/src/main/java/Tienda_Celeste/demo/dao/RolDao.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Tienda_Celeste.demo.dao;
+import Tienda_Celeste.demo.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol, Long> {
+
+}
--- Tienda_CelesteSh/src/main/java/Tienda_Celeste/demo/dao/UsuarioDao.java
+++ Tienda_CelesteSh/src/main/java/Tienda_Celeste/demo/dao/UsuarioDao.java
@@ -7,6 +7,12 @@ package Tienda_Celeste.demo.dao;
 import Tienda_Celeste.demo.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioDao extends JpaRepository<Usuario, Long> {    
+public interface UsuarioDao extends JpaRepository<Usuario, Long> {
     Usuario findByUsername(String username);
+    
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
 }
